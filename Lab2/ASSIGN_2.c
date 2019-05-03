a2be262a29397d8bfadc71164a92ff07bea37638
Prashun Chitrakar <prashuncool@gmail.com>
2019-05-03 19:45:45 +0545

    Refactoring

--- Lab2/ASSIGN_2.c
+++ Lab2/ASSIGN_2.c
@@ -2,23 +2,23 @@
 #include <math.h>
 
 int main(){
-	int x[] = {2, 4, 6, 8, 10, 12}, i, j, k=0;
-	float y[6][6] = {{5.1, 4.2, 3.1, 3.5, 6.2, 7.3}};
-	for(i=1; i<6; i++){
-		for(j=0; j<6-k; j++){
-			y[i][j] = y[i-1][j+1] - y[i-1][j];
-		}
-		++k;
-	}
-	k=0;
-	printf("x\ty\tIFD\tIIFD\tIIIFD\tIVFD\tVFD\n");
-	for(i=0; i<6; i++){
-		printf("%d\t", x[i]);
-		for(j=0; j<6-k; j++){
-			printf("%.3f\t", y[j][i]);
-		}
-		printf("\n");
-		++k;
-	}
-	return 0;
+    int x[] = {2, 4, 6, 8, 10, 12}, i, j, k=0;
+    float y[6][6] = {{5.1, 4.2, 3.1, 3.5, 6.2, 7.3}};
+    for(i=1; i<6; i++){
+        for(j=0; j<6-k; j++){
+            y[i][j] = y[i-1][j+1] - y[i-1][j];
+        }
+        ++k;
+    }
+    k=0;
+    printf("x\ty\tIFD\tIIFD\tIIIFD\tIVFD\tVFD\n");
+    for(i=0; i<6; i++){
+        printf("%d\t", x[i]);
+        for(j=0; j<6-k; j++){
+            printf("%.3f\t", y[j][i]);
+        }
+        printf("\n");
+        ++k;
+    }
+    return 0;
 }
--- Lab2/function table.c
+++ Lab2/function table.c
@@ -4,12 +4,12 @@
 #define G(b) (3*b*b - 4)
 
 int main(){
-	float a = 0.0, b=0.0;
-	printf("a\tb\tF(a)\t\tG(b)\n");
-	while(b!=5.0){
-		printf("%.2f\t%.2f\t%f\t%f\n", a, b, F(a), G(b));
-		a+=0.25;
-		b+=0.5;
-	}
-	return 0;
+    float a = 0.0, b=0.0;
+    printf("a\tb\tF(a)\t\tG(b)\n");
+    while(b!=5.0){
+        printf("%.2f\t%.2f\t%f\t%f\n", a, b, F(a), G(b));
+        a+=0.25;
+        b+=0.5;
+    }
+    return 0;
 }
