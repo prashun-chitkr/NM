a4fd12d524adbc7a9cbc6c0e972ad80c310eeeb8
Prashun Chitrakar <prashuncool@gmail.com>
2019-05-05 08:30:46 +0545

    Refactoring and optimization

--- Lab3/bisection.c
+++ Lab3/bisection.c
@@ -2,28 +2,44 @@
 #include <stdlib.h>
 #include <math.h>
 
-#define F(x) (exp(-x) * (3.2*sin(x) - 0.5*cos(x)))
+#define F(x) (x*x*x + x*x + x + 7)
 #define PRECISION 0.0001
 
 int main(){;
-    double a, b, m, y_a, y_m, y_b;
-    int i=1;
-    printf("-------------------\n");
-    printf("Enter range: ");
-    scanf("%lf %lf", &a, &b);
+    double a, b, m, y_a, y_m, y_b, tmp;
+    int i=1, iterations;
 
     printf("-------------------\n");
-    printf("Iterations: %.2f\n", (log10(b-a) - log10(PRECISION))/log10(2));
+
+    do{
+        a = b = 0.0;
+        printf("Enter range: ");
+        scanf("%lf %lf", &a, &b);
+    }while((F(a)*F(b))>0);
+
+    if(a>b){
+        tmp = a;
+        a = b;
+        b = tmp;
+    }
+    
+    printf("-------------------\n");
+
+    iterations = (int)(log10(b-a) - log10(PRECISION))/log10(2) + 1;
+    printf("Iterations: %d\n", iterations);
     
     printf("------------------------------------------------------------------------\n");
+
     printf("| n    | %-7c | %-7c | %-7.9s | %-7.9s  | %-7c | %-7.9s    |\n", 'a', 'b', "F(a)", "F(b)", 'x', "F(x)");
+
     printf("------------------------------------------------------------------------\n");
-    while(b - a > PRECISION){
+
+    while(i <= iterations){
             m = (a+b) / 2;
             y_a = F(a);
             y_b = F(b);
             y_m = F(m);
-            printf("| %3d. | %7.5f | %7.5f | %7.5f | %7.5f | %7.5f | %8.5f   |\n", i, a, b, y_a, y_b, m, y_m);
+            printf("| %3d. | %7.4f | %7.4f | %7.4f | %7.4f | %7.4f | %8.4f   |\n", i, a, b, y_a, y_b, m, y_m);
             if(y_b * y_m >= 0)
                 b = m;
             else if(y_a * y_m >=0)
@@ -32,7 +48,8 @@ int main(){;
     }
     printf("------------------------------------------------------------------------\n");
     
-    printf("Approx Solution: %lf\n", (a+b)/2);
+    printf("Approx Solution: %4.3lf\n", (a+b)/2);
+
     printf("-------------------------------\n");
     return 0;
 }
